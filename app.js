c76c0c87dd81efe20f3b7f27e30d2362893ceb61
Michae Ion <michael.ion.mi@outlook.com>
2022-08-28 22:57:29 -0400

    basic test res.send '/' page working

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
+app.get('/', (_req, res) => {
+    res.send('Home')
+})
+
+
 app.listen(port, () => console.log(`listening on port ${port}`))
