0f851ac7260bbd1b97b2bb2610f7acb6ac4c4d39
Michae Ion <michael.ion.mi@outlook.com>
2022-08-28 23:13:01 -0400

    set up ejs view engine linked css working

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import express from 'express'
+import path from 'path'
+import {fileURLToPath} from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
 const port = process.env.PORT || 3000
 
+// middleware
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (_req, res) => {
-    res.send('Home')
+    res.render('gamex')
 })
 
 
