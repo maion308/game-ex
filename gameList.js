20d5a552bb685ba5898d543103876e2dd5c0e71d
Michae Ion <michael.ion.mi@outlook.com>
2022-08-30 10:35:58 -0400

    added images to gitignore

--- gameList.js
+++ gameList.js
@@ -1,34 +1,94 @@
 export const gameList = [
     {
         name: 'Hogwarts Legacy',
-        img: '/hogwarts.jpeg'
+        img: '/images/hogwarts.jpeg'
     },
     {
         name: 'Dead Island 2',
-        img: '/deadisland2.jpeg'
+        img: '/images/deadisland2.jpeg'
     },
     {
         name: 'Deliver Us Mars',
-        img: '/deliverusmars.jpeg'
+        img: '/images/deliverusmars.jpeg'
     },
     {
         name: 'Forspoken',
-        img: '/forspoken.jpeg'
+        img: '/images/forspoken.jpeg'
     },
     {
         name: 'Hello Neighbor 2',
-        img: '/helloneighbor2.jpeg'
+        img: '/images/helloneighbor2.jpeg'
     },
     {
         name: 'The Dark Pictures Anthology: The Devil in Me',
-        img: '/darkpicturesanthology.jpeg'
+        img: '/images/darkpicturesanthology.jpeg'
     },
     {
         name: 'Goat Simulator 3',
-        img: '/goatsimulator3.jpeg'
+        img: '/images/goatsimulator3.jpeg'
     },
     {
         name: 'Farming Simulator 22',
-        img: '/farmingsimulator22.jpeg'
-    }
+        img: '/images/farmingsimulator22.jpeg'
+    },
+    {
+        name: 'God of War Ragnarök',
+        img: '/images/godofwar.jpeg'
+    },
+    {
+        name: 'Gotham Knights',
+        img: '/images/gothamknights.jpeg'
+    },
+    {
+        name: 'Ghostbusters: Spirits Unleashed',
+        img: '/images/ghostbusters.jpeg'
+    },
+    {
+        name: 'NBA 2K23',
+        img: '/images/nbask23.jpeg'
+    },
+    {
+        name: 'The Last of Us™ Part I',
+        img: '/images/lastofuspart1.jpeg'
+    },
+    {
+        name: 'Aquadine',
+        img: '/images/aquadine.jpeg'
+    },
+     {
+        name: 'Witch College Bundle',
+        img: '/images/witchcollege.jpeg'
+     },
+     {
+        name: 'Saints Row ',
+        img: '/images/saintsrow.jpeg'
+     },
+     {
+        name: 'Way of the Hunter',
+        img: '/images/wayofthehunter.jpeg'
+     },
+     {
+        name: 'Stray',
+        img: '/images/stray.jpeg'
+     },
+     {
+        name: 'Ghostwire: Tokyo',
+        img: '/images/ghostwiretokyo.jpeg'
+     },
+     {
+        name: 'Cyberpunk 2077',
+        img: '/images/cyberpunk2077.jpeg'
+     },
+     {
+        name: 'Fortnite',
+        img: '/images/fortnite.jpeg'
+     },
+     {
+        name: 'Dying Light 2 Stay Human',
+        img: '/images/dyinglight2.jpeg'
+     },
+     {
+        name: 'Call of Duty®: Modern Warfare® II',
+        img: '/images/codwarfare.jpeg'
+     }
 ]
