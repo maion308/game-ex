f4d14eda5d858881d869fed06ca33fd86044f0d6
Michae Ion <michael.ion.mi@outlook.com>
2022-08-29 22:22:53 -0400

    Added: css, ejs design and functionality to post reviews using arrays for storage

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 import express from 'express'
 import path from 'path'
 import {fileURLToPath} from 'url';
+import {gameList} from './gameList.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express()
 const port = process.env.PORT || 3000
 
+const reviews = []
+
 // middleware
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (_req, res) => {
-    res.render('gamex')
+    for(const title of gameList) {
+        if(title.name === reviews.gameTitle) {
+            console.log("found a match")
+            console.log(title)
+        }
+    }
+    res.render('gamex', {reviews: reviews, gameList: gameList})
+})
+
+app.post('/', (req, res) => {
+    const result = req.body
+    console.log(result)
+    reviews.push(result)
+    res.redirect('/')
+})
+
+app.get('/search', (_req, res) => {
+    res.send('Work In Progress Check Back Soon!')
 })
 
 
---
+++ gameList.js
@@ -0,0 +1,34 @@
+export const gameList = [
+    {
+        name: 'Hogwarts Legacy',
+        img: '/hogwarts.jpeg'
+    },
+    {
+        name: 'Dead Island 2',
+        img: '/deadisland2.jpeg'
+    },
+    {
+        name: 'Deliver Us Mars',
+        img: '/deliverusmars.jpeg'
+    },
+    {
+        name: 'Forspoken',
+        img: '/forspoken.jpeg'
+    },
+    {
+        name: 'Hello Neighbor 2',
+        img: '/helloneighbor2.jpeg'
+    },
+    {
+        name: 'The Dark Pictures Anthology: The Devil in Me',
+        img: '/darkpicturesanthology.jpeg'
+    },
+    {
+        name: 'Goat Simulator 3',
+        img: '/goatsimulator3.jpeg'
+    },
+    {
+        name: 'Farming Simulator 22',
+        img: '/farmingsimulator22.jpeg'
+    }
+]
