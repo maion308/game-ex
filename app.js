6ae23e80ab611a7d691930f70fbdb8fe36c6f20b
Michae Ion <michael.ion.mi@outlook.com>
2022-08-30 12:49:06 -0400

    connect to mongodb, retrieve data from 2 collections insert reviews added to mongodb and displaying successfully

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ import express from 'express'
 import path from 'path'
 import {fileURLToPath} from 'url';
 import {gameList} from './gameList.js'
+import { GameList, Review}  from './mongoUtil.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express()
 const port = process.env.PORT || 3000
 
-const reviews = []
 
 // middleware
 app.set('view engine', 'ejs')
@@ -18,20 +18,34 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.get('/', (_req, res) => {
-    for(const title of gameList) {
-        if(title.name === reviews.gameTitle) {
-            console.log("found a match")
-            console.log(title)
-        }
-    }
-    res.render('gamex', {reviews: reviews, gameList: gameList})
+    GameList.find({}, (_error, foundItems)=> {
+        Review.find({}, (_error, reviewItems)=> {
+            if(foundItems.length === 0) {
+                GameList.insertMany(gameList, (error) => {
+                    if(error) {
+                        console.log(error)
+                    } else {
+                        console.log('success')
+                    }
+                })
+                res.redirect('/')
+            } else {
+                res.render('gamex', {reviews: reviewItems, gameList: foundItems})
+            }
+        })
+    })
 })
 
 app.post('/', (req, res) => {
     const result = req.body
     console.log(result)
-    reviews.push(result)
-    res.redirect('/')
+    if(result === '') {res.redirect('/')}
+    else {
+        const review = new Review(result)
+        review.save()
+        res.redirect('/')
+    }
+    
 })
 
 app.get('/search', (_req, res) => {
--- mongoUtil.js
+++ mongoUtil.js
@@ -1,4 +1,19 @@
 import { mongoose } from 'mongoose'
+import { gameList } from './gameList.js'
 
 mongoose.connect('mongodb://localhost:27017/gamexpedia')
 
+const gamesSchema = {
+    name: String,
+    img: String
+}
+
+export const GameList = mongoose.model('GameList', gamesSchema)
+
+const reviewsSchema = {
+    game: String,
+    review: String,
+    rating: Number 
+}
+
+export const Review = mongoose.model('Review', reviewsSchema)
